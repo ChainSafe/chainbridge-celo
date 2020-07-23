1aeca491f124fe62261dcd601f7d010671816867
David Ansermino <david@chainsafe.io>
2020-07-23 09:20:15 -0400

    Copy over scripts, rebuild bindings

---
+++ bindings/ERC20Safe/ERC20Safe.go
@@ -0,0 +1,262 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package ERC20Safe
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// ERC20SafeABI is the input ABI used to generate the binding from.
+const ERC20SafeABI = "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"_balances\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"_burnedTokens\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"tokenAddress\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"fundERC20\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+
+// ERC20SafeBin is the compiled bytecode used for deploying new contracts.
+var ERC20SafeBin = "0x608060405234801561001057600080fd5b506105c0806100206000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c806345274738146100465780636ebcf6071461009e57806395601f09146100f6575b600080fd5b6100886004803603602081101561005c57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610164565b6040518082815260200191505060405180910390f35b6100e0600480360360208110156100b457600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919050505061017c565b6040518082815260200191505060405180910390f35b6101626004803603606081101561010c57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190505050610194565b005b60016020528060005260406000206000915090505481565b60006020528060005260406000206000915090505481565b60008390506101a58184308561023e565b6101f6826000808773ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000205461032b90919063ffffffff16565b6000808673ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000208190555050505050565b610325846323b872dd60e01b858585604051602401808473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020018373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020018281526020019350505050604051602081830303815290604052907bffffffffffffffffffffffffffffffffffffffffffffffffffffffff19166020820180517bffffffffffffffffffffffffffffffffffffffffffffffffffffffff83818316178352505050506103b3565b50505050565b6000808284019050838110156103a9576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601b8152602001807f536166654d6174683a206164646974696f6e206f766572666c6f77000000000081525060200191505060405180910390fd5b8091505092915050565b600060608373ffffffffffffffffffffffffffffffffffffffff16836040518082805190602001908083835b6020831061040257805182526020820191506020810190506020830392506103df565b6001836020036101000a0380198251168184511680821785525050505050509050019150506000604051808303816000865af19150503d8060008114610464576040519150601f19603f3d011682016040523d82523d6000602084013e610469565b606091505b5091509150816104e1576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260128152602001807f45524332303a2063616c6c206661696c6564000000000000000000000000000081525060200191505060405180910390fd5b6000815111156105845780806020019051602081101561050057600080fd5b8101908080519060200190929190505050610583576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260208152602001807f45524332303a206f7065726174696f6e20646964206e6f74207375636365656481525060200191505060405180910390fd5b5b5050505056fea2646970667358221220d2781de6ba22dfddd0c067b849f776b24f14ab3d34022dcad54d717592df38f964736f6c63430006040033"
+
+// DeployERC20Safe deploys a new Ethereum contract, binding an instance of ERC20Safe to it.
+func DeployERC20Safe(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *ERC20Safe, error) {
+	parsed, err := abi.JSON(strings.NewReader(ERC20SafeABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(ERC20SafeBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &ERC20Safe{ERC20SafeCaller: ERC20SafeCaller{contract: contract}, ERC20SafeTransactor: ERC20SafeTransactor{contract: contract}, ERC20SafeFilterer: ERC20SafeFilterer{contract: contract}}, nil
+}
+
+// ERC20Safe is an auto generated Go binding around an Ethereum contract.
+type ERC20Safe struct {
+	ERC20SafeCaller     // Read-only binding to the contract
+	ERC20SafeTransactor // Write-only binding to the contract
+	ERC20SafeFilterer   // Log filterer for contract events
+}
+
+// ERC20SafeCaller is an auto generated read-only Go binding around an Ethereum contract.
+type ERC20SafeCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ERC20SafeTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type ERC20SafeTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ERC20SafeFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type ERC20SafeFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ERC20SafeSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type ERC20SafeSession struct {
+	Contract     *ERC20Safe        // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// ERC20SafeCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type ERC20SafeCallerSession struct {
+	Contract *ERC20SafeCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts    // Call options to use throughout this session
+}
+
+// ERC20SafeTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type ERC20SafeTransactorSession struct {
+	Contract     *ERC20SafeTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts    // Transaction auth options to use throughout this session
+}
+
+// ERC20SafeRaw is an auto generated low-level Go binding around an Ethereum contract.
+type ERC20SafeRaw struct {
+	Contract *ERC20Safe // Generic contract binding to access the raw methods on
+}
+
+// ERC20SafeCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type ERC20SafeCallerRaw struct {
+	Contract *ERC20SafeCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// ERC20SafeTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type ERC20SafeTransactorRaw struct {
+	Contract *ERC20SafeTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewERC20Safe creates a new instance of ERC20Safe, bound to a specific deployed contract.
+func NewERC20Safe(address common.Address, backend bind.ContractBackend) (*ERC20Safe, error) {
+	contract, err := bindERC20Safe(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &ERC20Safe{ERC20SafeCaller: ERC20SafeCaller{contract: contract}, ERC20SafeTransactor: ERC20SafeTransactor{contract: contract}, ERC20SafeFilterer: ERC20SafeFilterer{contract: contract}}, nil
+}
+
+// NewERC20SafeCaller creates a new read-only instance of ERC20Safe, bound to a specific deployed contract.
+func NewERC20SafeCaller(address common.Address, caller bind.ContractCaller) (*ERC20SafeCaller, error) {
+	contract, err := bindERC20Safe(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ERC20SafeCaller{contract: contract}, nil
+}
+
+// NewERC20SafeTransactor creates a new write-only instance of ERC20Safe, bound to a specific deployed contract.
+func NewERC20SafeTransactor(address common.Address, transactor bind.ContractTransactor) (*ERC20SafeTransactor, error) {
+	contract, err := bindERC20Safe(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ERC20SafeTransactor{contract: contract}, nil
+}
+
+// NewERC20SafeFilterer creates a new log filterer instance of ERC20Safe, bound to a specific deployed contract.
+func NewERC20SafeFilterer(address common.Address, filterer bind.ContractFilterer) (*ERC20SafeFilterer, error) {
+	contract, err := bindERC20Safe(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &ERC20SafeFilterer{contract: contract}, nil
+}
+
+// bindERC20Safe binds a generic wrapper to an already deployed contract.
+func bindERC20Safe(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(ERC20SafeABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_ERC20Safe *ERC20SafeRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _ERC20Safe.Contract.ERC20SafeCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_ERC20Safe *ERC20SafeRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _ERC20Safe.Contract.ERC20SafeTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_ERC20Safe *ERC20SafeRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _ERC20Safe.Contract.ERC20SafeTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_ERC20Safe *ERC20SafeCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _ERC20Safe.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_ERC20Safe *ERC20SafeTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _ERC20Safe.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_ERC20Safe *ERC20SafeTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _ERC20Safe.Contract.contract.Transact(opts, method, params...)
+}
+
+// Balances is a free data retrieval call binding the contract method 0x6ebcf607.
+//
+// Solidity: function _balances(address ) view returns(uint256)
+func (_ERC20Safe *ERC20SafeCaller) Balances(opts *bind.CallOpts, arg0 common.Address) (*big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+	)
+	out := ret0
+	err := _ERC20Safe.contract.Call(opts, out, "_balances", arg0)
+	return *ret0, err
+}
+
+// Balances is a free data retrieval call binding the contract method 0x6ebcf607.
+//
+// Solidity: function _balances(address ) view returns(uint256)
+func (_ERC20Safe *ERC20SafeSession) Balances(arg0 common.Address) (*big.Int, error) {
+	return _ERC20Safe.Contract.Balances(&_ERC20Safe.CallOpts, arg0)
+}
+
+// Balances is a free data retrieval call binding the contract method 0x6ebcf607.
+//
+// Solidity: function _balances(address ) view returns(uint256)
+func (_ERC20Safe *ERC20SafeCallerSession) Balances(arg0 common.Address) (*big.Int, error) {
+	return _ERC20Safe.Contract.Balances(&_ERC20Safe.CallOpts, arg0)
+}
+
+// BurnedTokens is a free data retrieval call binding the contract method 0x45274738.
+//
+// Solidity: function _burnedTokens(address ) view returns(uint256)
+func (_ERC20Safe *ERC20SafeCaller) BurnedTokens(opts *bind.CallOpts, arg0 common.Address) (*big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+	)
+	out := ret0
+	err := _ERC20Safe.contract.Call(opts, out, "_burnedTokens", arg0)
+	return *ret0, err
+}
+
+// BurnedTokens is a free data retrieval call binding the contract method 0x45274738.
+//
+// Solidity: function _burnedTokens(address ) view returns(uint256)
+func (_ERC20Safe *ERC20SafeSession) BurnedTokens(arg0 common.Address) (*big.Int, error) {
+	return _ERC20Safe.Contract.BurnedTokens(&_ERC20Safe.CallOpts, arg0)
+}
+
+// BurnedTokens is a free data retrieval call binding the contract method 0x45274738.
+//
+// Solidity: function _burnedTokens(address ) view returns(uint256)
+func (_ERC20Safe *ERC20SafeCallerSession) BurnedTokens(arg0 common.Address) (*big.Int, error) {
+	return _ERC20Safe.Contract.BurnedTokens(&_ERC20Safe.CallOpts, arg0)
+}
+
+// FundERC20 is a paid mutator transaction binding the contract method 0x95601f09.
+//
+// Solidity: function fundERC20(address tokenAddress, address owner, uint256 amount) returns()
+func (_ERC20Safe *ERC20SafeTransactor) FundERC20(opts *bind.TransactOpts, tokenAddress common.Address, owner common.Address, amount *big.Int) (*types.Transaction, error) {
+	return _ERC20Safe.contract.Transact(opts, "fundERC20", tokenAddress, owner, amount)
+}
+
+// FundERC20 is a paid mutator transaction binding the contract method 0x95601f09.
+//
+// Solidity: function fundERC20(address tokenAddress, address owner, uint256 amount) returns()
+func (_ERC20Safe *ERC20SafeSession) FundERC20(tokenAddress common.Address, owner common.Address, amount *big.Int) (*types.Transaction, error) {
+	return _ERC20Safe.Contract.FundERC20(&_ERC20Safe.TransactOpts, tokenAddress, owner, amount)
+}
+
+// FundERC20 is a paid mutator transaction binding the contract method 0x95601f09.
+//
+// Solidity: function fundERC20(address tokenAddress, address owner, uint256 amount) returns()
+func (_ERC20Safe *ERC20SafeTransactorSession) FundERC20(tokenAddress common.Address, owner common.Address, amount *big.Int) (*types.Transaction, error) {
+	return _ERC20Safe.Contract.FundERC20(&_ERC20Safe.TransactOpts, tokenAddress, owner, amount)
+}
