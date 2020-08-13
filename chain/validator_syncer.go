92bf23cb12c7713f31a5b038553ad282017f9c2a
∂lirezaly <66266605+alirezaly@users.noreply.github.com>
2020-08-13 21:37:29 +0700

    feat: aggregate public keys (#19)
    * feat: aggregate public keys
    
    * refactor: generate validators instead of using local for tests
    
    * fix: check transaction hash lives when retriving from the block

--- chain/validator_syncer.go
+++ chain/validator_syncer.go
@@ -8,13 +8,16 @@ import (
 	"math/big"
 
 	"github.com/ChainSafe/chainbridge-celo/connection"
+	"github.com/celo-org/celo-bls-go/bls"
 	"github.com/ethereum/go-ethereum/consensus/istanbul"
 	"github.com/ethereum/go-ethereum/consensus/istanbul/validator"
+	blscrypto "github.com/ethereum/go-ethereum/crypto/bls"
 	"github.com/pkg/errors"
 )
 
 type ValidatorSyncer struct {
-	conn *connection.Connection
+	conn       *connection.Connection
+	validators []istanbul.ValidatorData
 }
 
 // ExtractValidators pulls the extra data from the block header and extract
@@ -26,6 +29,32 @@ func (v *ValidatorSyncer) ExtractValidators(num uint64) ([]istanbul.ValidatorDat
 	}
 
 	return validator.ExtractValidators(header.Extra), nil
+}
+
+// AggregatePublicKeys merges all the validators public keys into one
+// and returns it as an aggeragated public key
+func (v *ValidatorSyncer) AggregatePublicKeys() (*bls.PublicKey, error) {
+	var publicKeys []blscrypto.SerializedPublicKey
+	for _, validator := range v.validators {
+		publicKeys = append(publicKeys, validator.BLSPublicKey)
+	}
+
+	publicKeyObjs := []*bls.PublicKey{}
+	for _, publicKey := range publicKeys {
+		publicKeyObj, err := bls.DeserializePublicKeyCached(publicKey[:])
+		if err != nil {
+			return nil, err
+		}
+		defer publicKeyObj.Destroy()
+		publicKeyObjs = append(publicKeyObjs, publicKeyObj)
+	}
+	apk, err := bls.AggregatePublicKeys(publicKeyObjs)
+	if err != nil {
+		return nil, err
+	}
+	defer apk.Destroy()
+
+	return apk, nil
 }
 
 func (v *ValidatorSyncer) start() error {
