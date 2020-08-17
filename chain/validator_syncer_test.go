46ba03efa3b9c95f7728b89661599d0af058bd6e
steviezhang <stephanie@chainsafe.io>
2020-08-17 19:11:39 -0400

    ipr

--- chain/validator_syncer_test.go
+++ chain/validator_syncer_test.go
@@ -4,30 +4,40 @@
 package chain
 
 import (
+	// "fmt"
 	"math/big"
 	"testing"
+	// "encoding/hex"
 
 	"github.com/ChainSafe/chainbridge-celo/connection"
 	"github.com/ChainSafe/chainbridge-utils/keystore"
 	"github.com/ChainSafe/log15"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus/istanbul/validator"
-	"github.com/ethereum/go-ethereum/crypto"
-	blscrypto "github.com/ethereum/go-ethereum/crypto/bls"
+	// "github.com/ethereum/go-ethereum/consensus/istanbul/validator"
+	// "github.com/ethereum/go-ethereum/crypto"
+	// blscrypto "github.com/ethereum/go-ethereum/crypto/bls"
 )
 
 var TestEndpoint = "ws://localhost:8545"
-var AliceKp = keystore.TestKeyRing.EthereumKeys[keystore.AliceKey]
 var GasLimit = big.NewInt(connection.DefaultGasLimit)
 var GasPrice = big.NewInt(connection.DefaultGasPrice)
+
+var AliceKp = keystore.TestKeyRing.EthereumKeys[keystore.AliceKey]
 
 var testAddresses = []common.Address{
 	// testAddresses are packed into our extra data
 	// this references `extraData` from our genesis.json
-	common.HexToAddress("0xecc833a7747eaa8327335e8e0c6b6d8aa3a38d00"),
-	common.HexToAddress("0x82c07B76ee5D6a5Ec4bA710418ae299d3bdCE703"),
-	common.HexToAddress("0x0000000000000000000000000000000000000000"),
+	common.HexToAddress("0xf4314cb9046bece6aa54bb9533155434d0c76909"),
+	common.HexToAddress("0x1da2d666868b7b1caff3f51c60f7b5e73ea57232"),
+	common.HexToAddress("0xcacf63aff9e92c22b0315ba8a0052b98c2a6b328"),
 }
+
+var expectedBlsPublicKeys = []string {
+	"0xec0d01b5adf993cdfee480b43be638b346ca58bc7d63d2d0e8b288de24bb320c02fa254a79fecc14511dc176f4e15c012e7d1b8ea9717c82c07b76ee5d6a5ec4ba710418ae299d3bdce703351f7c465fbaeb7ba814b43d7206546051d90f1b80",
+	"0xb4107f0ce86e610bdde19c273c5f51776bbc48c0a685741a13d6f70a78e5bb931c2357601fe10bbac66b2a14e6778301863c678d1ece07b7c7bd46550d076625714f93600146393c50577d28924e2881b15a5f7bc585f8cb0ef501fd4503ac00",
+	"0x1549869e84beed58692471c042776e39fcdc32ffd10663d3d3fb6793c9705a272e09311469f92d7765070ec196d4380085ad2fb742ebf5d57d252cf4a1d21c6d5a2ddf4190a3a122bc0167e5307bd90c83109b6609ddf5ce287d56ef43601b00",
+}
+
 
 func createTestConnection(t *testing.T) *connection.Connection {
 	conn := connection.NewConnection(TestEndpoint, false, AliceKp, log15.Root(), GasLimit, GasPrice)
@@ -65,21 +75,9 @@ func TestValidatorSyncer_AggregatePublicKeys(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	const ValCnt = 100
+	// const ValCnt = 3
 
-	// Create 100 validators with random addresses
-	b := []byte{}
-	for i := 0; i < ValCnt; i++ {
-		key, _ := crypto.GenerateKey()
-		blsPrivateKey, _ := blscrypto.ECDSAToBLS(key)
-		blsPublicKey, _ := blscrypto.PrivateToPublic(blsPrivateKey)
-		addr := crypto.PubkeyToAddress(key.PublicKey)
-		val := validator.New(addr, blsPublicKey)
-		b = append(b, val.Address().Bytes()...)
-		b = append(b, blsPublicKey[:]...)
-	}
-
-	vsyncer.validators = validator.ExtractValidators(b)
+	// vsyncer.validators = validator.ExtractValidators(b)
 
 	_, err = vsyncer.AggregatePublicKeys()
 	if err != nil {
