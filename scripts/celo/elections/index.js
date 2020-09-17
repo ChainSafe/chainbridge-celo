747e64a55b7f529995148a3d8272a96b3d5a3596
Oleksii Matiiasevych <oleksii@ambisafe.com>
2020-09-17 07:46:42 -0700

    Add license to elections script

--- scripts/celo/elections/index.js
+++ scripts/celo/elections/index.js
@@ -1,3 +1,6 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
 const { privateKeyToAddress, privateKeyToPublicKey, trimLeading0x } = require('@celo/utils/lib/address');
 const { getBlsPoP, getBlsPublicKey } = require('@celo/utils/lib/bls');
 const Web3 = require('web3');
