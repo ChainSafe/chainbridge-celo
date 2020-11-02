96422efa5caa055e08be938bc8db627940aa49ea
YUSUF <yusufkel@gmail.com>
2020-11-02 17:10:31 +0400

    added chain/writer_test.go

--- chain/writer_test.go
+++ chain/writer_test.go
@@ -4,9 +4,19 @@
 package chain  
 
 import (
+	"context"
+	"math/big"
 	"testing"
-	ethtest "github.com/ChainSafe/ChainBridge/shared/ethereum/testing"
+	"time"
+	"github.com/ChainSafe/log15"
+	"github.com/ChainSafe/chainbridge-utils/msg"
+	ethtest "github.com/ChainSafe/chainbridge-celo/shared/ethereum/testing"
 	utils "github.com/ChainSafe/chainbridge-celo/shared/ethereum"
+	"github.com/ChainSafe/chainbridge-celo/bindings/Bridge"
+	eth "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
 )
 
 func createWriters(t *testing.T, client *utils.Client, contracts *utils.DeployedContracts) (*writer, *writer, func(), func(), chan error, chan error) {
@@ -111,4 +121,153 @@ func TestWriter_start_stop(t *testing.T) {
 
 	// Initiate shutdown
 	close(stop)
+}
+
+func TestCreateAndExecuteErc721Proposal(t *testing.T) {
+	client := ethtest.NewClient(t, TestEndpoint, AliceKp)
+	contracts := deployTestContracts(t, client, TestChainId)
+	writerA, writerB, stopA, stopB, errA, errB := createWriters(t, client, contracts)
+
+	defer stopA()
+	defer stopB()
+	defer writerA.conn.Close()
+	defer writerB.conn.Close()
+
+	// We'll use alice to setup the erc721
+	tokenId := big.NewInt(1)
+	erc721Contract := ethtest.Erc721Deploy(t, client)
+	ethtest.Erc721Mint(t, client, erc721Contract, tokenId, []byte{})
+	ethtest.Erc721FundHandler(t, client, contracts.ERC721HandlerAddress, erc721Contract, tokenId)
+
+	// Create initial transfer message
+	resourceId := msg.ResourceIdFromSlice(append(common.LeftPadBytes(erc721Contract.Bytes(), 31), 0))
+	recipient := ethcrypto.PubkeyToAddress(BobKp.PrivateKey().PublicKey)
+	m := msg.NewNonFungibleTransfer(1, 0, 0, resourceId, tokenId, recipient.Bytes(), []byte{})
+	ethtest.RegisterResource(t, client, contracts.BridgeAddress, contracts.ERC721HandlerAddress, resourceId, erc721Contract)
+	// Helpful for debugging
+	go ethtest.WatchEvent(client, contracts.BridgeAddress, utils.ProposalEvent)
+	go ethtest.WatchEvent(client, contracts.BridgeAddress, utils.ProposalVote)
+
+	routeMessageAndWait(t, client, writerA, writerB, m, errA, errB)
+
+	ethtest.Erc721AssertOwner(t, client, erc721Contract, tokenId, recipient)
+}
+
+func TestCreateAndExecuteGenericProposal(t *testing.T) {
+	client := ethtest.NewClient(t, TestEndpoint, AliceKp)
+	contracts := deployTestContracts(t, client, TestChainId)
+	writerA, writerB, stopA, stopB, errA, errB := createWriters(t, client, contracts)
+
+	defer stopA()
+	defer stopB()
+	defer writerA.conn.Close()
+	defer writerB.conn.Close()
+
+	assetStoreAddr, err := utils.DeployAssetStore(client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rId := msg.ResourceIdFromSlice(common.LeftPadBytes(assetStoreAddr.Bytes(), 32))
+	depositSig := utils.CreateFunctionSignature("")
+	executeSig := utils.CreateFunctionSignature("store(bytes32)")
+
+	ethtest.RegisterGenericResource(t, client, contracts.BridgeAddress, contracts.GenericHandlerAddress, rId, assetStoreAddr, depositSig, executeSig)
+	// Create initial transfer message
+	hash := common.HexToHash("0xf0a8748d2b102eb4e0e116047753b9beff0396d81b830693b19a1376ac4b14e8")
+	m := msg.Message{
+		Source:       1,
+		Destination:  0,
+		Type:         msg.GenericTransfer,
+		DepositNonce: 0,
+		ResourceId:   rId,
+		Payload: []interface{}{
+			hash.Bytes(),
+		},
+	}
+
+	// Helpful for debugging
+	go ethtest.WatchEvent(client, contracts.BridgeAddress, utils.ProposalEvent)
+	go ethtest.WatchEvent(client, contracts.BridgeAddress, utils.ProposalVote)
+	routeMessageAndWait(t, client, writerA, writerB, m, errA, errB)
+
+	ethtest.AssertHashExistence(t, client, hash, assetStoreAddr)
+}
+
+func TestDuplicateMessage(t *testing.T) {
+	client := ethtest.NewClient(t, TestEndpoint, AliceKp)
+	contracts := deployTestContracts(t, client, TestChainId)
+	writerA, writerB, stopA, stopB, errA, errB := createWriters(t, client, contracts)
+
+	defer stopA()
+	defer stopB()
+	defer writerA.conn.Close()
+	defer writerB.conn.Close()
+
+	erc20Address := ethtest.DeployMintApproveErc20(t, client, contracts.ERC20HandlerAddress, big.NewInt(100))
+	ethtest.FundErc20Handler(t, client, contracts.ERC20HandlerAddress, erc20Address, big.NewInt(100))
+
+	// Create initial transfer message
+	resourceId := msg.ResourceIdFromSlice(append(common.LeftPadBytes(erc20Address.Bytes(), 31), 0))
+	recipient := ethcrypto.PubkeyToAddress(BobKp.PrivateKey().PublicKey)
+	amount := big.NewInt(10)
+	m := msg.NewFungibleTransfer(1, 0, 10, amount, resourceId, recipient.Bytes())
+	ethtest.RegisterResource(t, client, contracts.BridgeAddress, contracts.ERC20HandlerAddress, resourceId, erc20Address)
+
+	data := ConstructErc20ProposalData(m.Payload[0].([]byte), m.Payload[1].([]byte))
+	dataHash := utils.Hash(append(contracts.ERC20HandlerAddress.Bytes(), data...))
+
+	// Helpful for debugging
+	go ethtest.WatchEvent(client, contracts.BridgeAddress, utils.ProposalEvent)
+	go ethtest.WatchEvent(client, contracts.BridgeAddress, utils.ProposalVote)
+
+	// Process initial message
+	routeMessageAndWait(t, client, writerA, writerB, m, errA, errB)
+
+	ethtest.Erc20AssertBalance(t, client, amount, erc20Address, recipient)
+
+	// Capture nonces
+	nonceAPre, err := writerA.conn.Client().PendingNonceAt(context.Background(), writerA.conn.Keypair().CommonAddress())
+	if err != nil {
+		t.Fatal(err)
+	}
+	nonceBPre, err := writerA.conn.Client().PendingNonceAt(context.Background(), writerB.conn.Keypair().CommonAddress())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Try processing the same message again
+	if ok := writerA.ResolveMessage(m); ok {
+		t.Fatalf("%s should have not voted", writerA.cfg.name)
+	}
+	if ok := writerB.ResolveMessage(m); ok {
+		t.Fatalf("%s should have not voted", writerB.cfg.name)
+	}
+
+	// Ensure the votes are recorded
+	if !writerA.hasVoted(m.Source, m.DepositNonce, dataHash) {
+		t.Fatal("Relayer vote not found on chain")
+	}
+	if !writerB.hasVoted(m.Source, m.DepositNonce, dataHash) {
+		t.Fatal("Relayer vote not found on chain")
+	}
+
+	// Capture new nonces
+	nonceAPost, err := writerA.conn.Client().PendingNonceAt(context.Background(), writerA.conn.Keypair().CommonAddress())
+	if err != nil {
+		t.Fatal(err)
+	}
+	nonceBPost, err := writerA.conn.Client().PendingNonceAt(context.Background(), writerB.conn.Keypair().CommonAddress())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Enssure neither relayer has submitted a tx
+	if nonceAPost != nonceAPre {
+		t.Error("Writer A nonce has changed.")
+	}
+	if nonceBPost != nonceBPre {
+		t.Error("Writer B nonce has changed.")
+	}
+
 }
