66492d96ecaa07d6bc813be33f7826a2fd1cd51a
YUSUF <yusufkel@gmail.com>
2020-11-02 17:02:40 +0400

    updated config

--- chain/config.go
+++ chain/config.go
@@ -5,14 +5,24 @@ package chain
 
 import (
 	"math/big"
+	"github.com/ChainSafe/chainbridge-utils/msg"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 type Config struct {
+	id                     msg.ChainId // ChainID
 	name                   string      // Human-readable chain name
+	endpoint               string      // url for rpc endpoint
+	from                   string      // address of key to use
+	keystorePath           string      // Location of keyfiles
+	blockstorePath         string
+	freshStart             bool // Disables loading from blockstore at start
 	bridgeContract         common.Address
 	erc20HandlerContract   common.Address
 	erc721HandlerContract  common.Address
 	genericHandlerContract common.Address
-	startBlock *big.Int
+	gasLimit               *big.Int
+	maxGasPrice            *big.Int
+	http                   bool // Config for type of connection
+	startBlock             *big.Int
 }
