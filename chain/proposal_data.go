b0d50061aa2c4cffc530d16435ed4045fc2dd6a5
YUSUF <yusufkel@gmail.com>
2020-11-05 15:00:23 +0400

    updated CreateProposalDataHash

--- chain/proposal_data.go
+++ chain/proposal_data.go
@@ -24,20 +24,16 @@ func ConstructErc20ProposalData(amount []byte, recipient []byte) []byte {
 }
 
 // constructErc721ProposalData returns the bytes to construct a proposal suitable for Erc721
-func ConstructErc721ProposalData(tokenId []byte, recipient []byte, metadata []byte) []byte {
-	var data []byte
-	data = append(data, common.LeftPadBytes(tokenId, 32)...) // tokenId ([]byte)
-
-	recipientLen := big.NewInt(int64(len(recipient))).Bytes()
-	data = append(data, common.LeftPadBytes(recipientLen, 32)...) // length of recipient
-	data = append(data, recipient...)                             // recipient ([]byte)
-
-	metadataLen := big.NewInt(int64(len(metadata))).Bytes()
-	data = append(data, common.LeftPadBytes(metadataLen, 32)...) // length of metadata (uint256)
-	data = append(data, metadata...)                             // metadata ([]byte)
-	return data
+func CreateProposalDataHash(data []byte, handler common.Address, extraData msg.MessageExtraData) [32]byte {
+	data = append(handler.Bytes(), data...)
+	data = append(data, extraData.RootHash[:]...)
+	data = append(data, extraData.Key...)
+	data = append(data, extraData.Nodes...)
+	data = append(data, extraData.AggregatePublicKey...)
+	data = append(data, extraData.HashedMessage...)
+	data = append(data, extraData.SignatureHeader...)
+	return utils.Hash(data)
 }
-
 // constructGenericProposalData returns the bytes to construct a generic proposal
 func ConstructGenericProposalData(metadata []byte) []byte {
 	var data []byte
