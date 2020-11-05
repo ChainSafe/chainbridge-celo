87b35d688f25e04b1b4d0c1a310857825d3294bb
YUSUF <yusufkel@gmail.com>
2020-11-05 15:33:13 +0400

    updated  chain/proposal_data.go

--- chain/proposal_data.go
+++ chain/proposal_data.go
@@ -44,6 +44,20 @@ func ConstructGenericProposalData(metadata []byte) []byte {
 	return data
 }
 
+func ConstructErc721ProposalData(tokenId []byte, recipient []byte, metadata []byte) []byte {
+	var data []byte
+	data = append(data, common.LeftPadBytes(tokenId, 32)...) // tokenId ([]byte)
+
+	recipientLen := big.NewInt(int64(len(recipient))).Bytes()
+	data = append(data, common.LeftPadBytes(recipientLen, 32)...) // length of recipient
+	data = append(data, recipient...)                             // recipient ([]byte)
+
+	metadataLen := big.NewInt(int64(len(metadata))).Bytes()
+	data = append(data, common.LeftPadBytes(metadataLen, 32)...) // length of metadata (uint256)
+	data = append(data, metadata...)                             // metadata ([]byte)
+	return data
+}
+
 func CreateErc721ProposalDataHash(data []byte, handler common.Address, extraData msg.MessageExtraData) [32]byte {
 	data = append(handler.Bytes(), data...)
 
