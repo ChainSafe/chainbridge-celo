f5cd4a967137b5cd30e6645820a424aa8e5a2bd0
YUSUF <yusufkel@gmail.com>
2020-11-11 20:14:46 +0400

    added cmd/main

---
+++ cmd/main.go
@@ -0,0 +1,130 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+/*
+Provides the command-line interface for the chainbridge-celo application.
+For configuration and CLI commands see the README: https://github.com/ChainSafe/chainbridge-celo
+*/
+package main
+
+import (
+
+	"os"
+	"strconv"
+	"github.com/ChainSafe/chainbridge-celo/config"
+	log "github.com/ChainSafe/log15"
+	"github.com/urfave/cli/v2"
+)
+
+var app = cli.NewApp()
+
+var cliFlags = []cli.Flag{
+	config.ConfigFileFlag,
+	config.VerbosityFlag,
+	config.KeystorePathFlag,
+	config.BlockstorePathFlag,
+	config.FreshStartFlag,
+	config.LatestBlockFlag,
+	config.MetricsFlag,
+	config.MetricsPort,
+}
+
+var generateFlags = []cli.Flag{
+	config.PasswordFlag,
+	config.Sr25519Flag,
+	config.Secp256k1Flag,
+	config.SubkeyNetworkFlag,
+}
+
+var devFlags = []cli.Flag{
+	config.TestKeyFlag,
+}
+
+var importFlags = []cli.Flag{
+	config.EthereumImportFlag,
+	config.PrivateKeyFlag,
+	config.Sr25519Flag,
+	config.Secp256k1Flag,
+	config.PasswordFlag,
+	config.SubkeyNetworkFlag,
+}
+
+var accountCommand = cli.Command{
+	Name:  "accounts",
+	Usage: "manage bridge keystore",
+	Description: "The accounts command is used to manage the bridge keystore.\n" +
+		"\tTo generate a new account (key type generated is determined on the flag passed in): chainbridge-celo accounts generate\n" +
+		"\tTo import a keystore file: chainbridge-celo accounts import path/to/file\n" +
+		"\tTo import a geth keystore file: chainbridge-celo accounts import --ethereum path/to/file\n" +
+		"\tTo import a private key file: chainbridge-celo accounts import --privateKey private_key\n" +
+		"\tTo list keys: chainbridge-celo accounts list",
+	Subcommands: []*cli.Command{
+		{
+			Action: wrapHandler(handleGenerateCmd),
+			Name:   "generate",
+			Usage:  "generate bridge keystore, key type determined by flag",
+			Flags:  generateFlags,
+			Description: "The generate subcommand is used to generate the bridge keystore.\n" +
+				"\tIf no options are specified, a secp256k1 key will be made.",
+		},
+		{
+			Action: wrapHandler(handleImportCmd),
+			Name:   "import",
+			Usage:  "import bridge keystore",
+			Flags:  importFlags,
+			Description: "The import subcommand is used to import a keystore for the bridge.\n" +
+				"\tA path to the keystore must be provided\n" +
+				"\tUse --ethereum to import an ethereum keystore from external sources such as geth\n" +
+				"\tUse --privateKey to create a keystore from a provided private key.",
+		},
+		{
+			Action:      wrapHandler(handleListCmd),
+			Name:        "list",
+			Usage:       "list bridge keystore",
+			Description: "The list subcommand is used to list all of the bridge keystores.\n",
+		},
+	},
+}
+
+// init initializes CLI
+func init() {
+	app.Action = run
+	app.Copyright = "Copyright 2019 ChainSafe Systems Authors"
+	app.Name = "chainbridge-celo"
+	app.Usage = "ChainBridge-celo"
+	app.Authors = []*cli.Author{{Name: "ChainSafe Systems 2019"}}
+	app.Version = "0.0.1"
+	app.EnableBashCompletion = true
+	app.Commands = []*cli.Command{
+		&accountCommand,
+	}
+
+	app.Flags = append(app.Flags, cliFlags...)
+	app.Flags = append(app.Flags, devFlags...)
+}
+
+func main() {
+	if err := app.Run(os.Args); err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func startLogger(ctx *cli.Context) error {
+	logger := log.Root()
+	handler := logger.GetHandler()
+	var lvl log.Lvl
+
+	if lvlToInt, err := strconv.Atoi(ctx.String(config.VerbosityFlag.Name)); err == nil {
+		lvl = log.Lvl(lvlToInt)
+	} else if lvl, err = log.LvlFromString(ctx.String(config.VerbosityFlag.Name)); err != nil {
+		return err
+	}
+	log.Root().SetHandler(log.LvlFilterHandler(lvl, handler))
+
+	return nil
+}
+
+func run(ctx *cli.Context) error {
+	// TODO - Implement run method
+	return nil
+}
