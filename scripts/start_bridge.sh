bc640ae9a08f7646eaa41e865752c335d36db336
Kirill <pisarevkir@gmail.com>
2021-02-15 17:46:08 +0200

    ga update

--- scripts/start_bridge.sh
+++ scripts/start_bridge.sh
@@ -3,6 +3,6 @@ set -ex
 
 
 echo "Running chainbridge..."
-./build/chainbridge-celo --config ./e2e/cfg/config-celo-int-tst.json --testkey alice --fresh --leveldb ./lvldb
+./build/chainbridge-celo --config ./e2e/cfg/config-celo-int-tst.json --testkey alice --fresh --leveldb ./lvldb &
 # Otherwise CI will run tests before ganache has started
 sleep 15
