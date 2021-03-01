c73f47199115a60c5a9945288fe4dfd896dbbe00
Kirill <pisarevkir@gmail.com>
2021-03-01 18:21:45 +0200

    covarage tunning

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CVPKG=$(go list ./... | grep -v 'cbcli\|e2e\|generated\|bindata\|mock\|main.go\|bindings\|shared\|root.go' | tr '\n' ',')
+CVPKG=$(go list ./... | grep -v 'cbcli\|e2e\|generated\|bindata\|mock\|main.go\|bindings\|shared\|root.go\|txs.go\|calls.go|' | tr '\n' ',')
 go test -coverpkg=$CVPKG -coverprofile=cover.out -p=1 $(go list ./... | grep -v 'cbcli\|e2e')
