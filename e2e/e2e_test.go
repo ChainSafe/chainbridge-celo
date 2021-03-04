29fd4b28f49e959470e082166450bb49c62fda10
Kirill <pisarevkir@gmail.com>
2021-03-04 13:45:39 +0200

    simulate test comented

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -190,15 +190,15 @@ func (s *IntegrationTestSuite) TestMultipleTransactionsInBlock() {
 }
 
 //nolint
-func (s *IntegrationTestSuite) TestSimulate() {
-	block := big.NewInt(380)
-	hash := common.HexToHash("0x4eb229f6aa1094545dc6cd9a1e031a270efd2b27b5e158947ee0ceaa8fc6fe8e")
-	res, err := utils.Simulate(s.client, block, hash, utils.AliceKp.CommonAddress())
-	s.Nil(err)
-	hexres := common.Bytes2Hex(res)
-	log.Info().Msgf("simulate result: %s", hexres)
-
-	rec, err := s.client.TransactionReceipt(context.TODO(), hash)
-	s.Nil(err)
-	log.Debug().Msgf("%+v", rec)
-}
+//func (s *IntegrationTestSuite) TestSimulate() {
+//	block := big.NewInt(380)
+//	hash := common.HexToHash("0x4eb229f6aa1094545dc6cd9a1e031a270efd2b27b5e158947ee0ceaa8fc6fe8e")
+//	res, err := utils.Simulate(s.client, block, hash, utils.AliceKp.CommonAddress())
+//	s.Nil(err)
+//	hexres := common.Bytes2Hex(res)
+//	log.Info().Msgf("simulate result: %s", hexres)
+//
+//	rec, err := s.client.TransactionReceipt(context.TODO(), hash)
+//	s.Nil(err)
+//	log.Debug().Msgf("%+v", rec)
+//}
