4cffe6dd2562e22f0ef7810178b1986b4f448a0b
Kirill <pisarevkir@gmail.com>
2021-03-22 21:30:39 +0200

    debugging e2e tests

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -7,11 +7,9 @@ import (
 	"testing"
 	"time"
 
-	erc20 "github.com/ChainSafe/chainbridge-celo/bindings/ERC20PresetMinterPauser"
 	"github.com/ChainSafe/chainbridge-celo/chain/client"
 	"github.com/ChainSafe/chainbridge-celo/utils"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -57,14 +55,14 @@ func (s *IntegrationTestSuite) TearDownTest() {}
 // Deposit hash: 0x42782f963df86c5f31f94d9c610445b72d388bd60f788e2cd8ea4bff17824426
 func (s *IntegrationTestSuite) TestDeposit() {
 	dstAddr := utils.BobKp.CommonAddress()
-	erc20Contract, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client.Client)
-	s.Nil(err)
-	erc20Contract2, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client2.Client)
-	s.Nil(err)
-	senderBalBefore, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
-	s.Nil(err)
-	destBalanceBefor, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
-	s.Nil(err)
+	//erc20Contract, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client.Client)
+	//s.Nil(err)
+	//erc20Contract2, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client2.Client)
+	//s.Nil(err)
+	//senderBalBefore, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
+	//s.Nil(err)
+	//destBalanceBefor, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
+	//s.Nil(err)
 
 	amountToDeposit := big.NewInt(1000000)
 	resourceID := utils.SliceTo32Bytes(append(common.LeftPadBytes(s.erc20ContractAddr.Bytes(), 31), uint8(5)))
@@ -72,7 +70,7 @@ func (s *IntegrationTestSuite) TestDeposit() {
 	s.Nil(err)
 	receipt, err := utils.WaitAndReturnTxReceipt(s.client, tx)
 	s.Nil(err)
-	log.Debug().Msg(tx.Hash().String())
+	//log.Debug().Msg(tx.Hash().String())
 
 	//Wait 30 seconds for relayer vote
 	time.Sleep(30 * time.Second)
@@ -90,101 +88,104 @@ func (s *IntegrationTestSuite) TestDeposit() {
 			passedEventFound = true
 		}
 	}
-	s.True(passedEventFound)
-
-	senderBalAfter, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
-	s.Nil(err)
-	s.Equal(senderBalBefore.Cmp(big.NewInt(0).Add(senderBalAfter, amountToDeposit)), 0)
-
-	//Wait 30 seconds for relayer to execute
-	time.Sleep(30 * time.Second)
-	lp, err = s.client2.LatestBlock()
-	s.Nil(err)
-	queryExecute := utils.BuildQuery(s.bridgeAddr, utils.ProposalEvent, receipt.BlockNumber, lp)
-	s.Nil(err)
-	evts2, err := s.client2.Client.FilterLogs(context.Background(), queryExecute)
-	var executedEventFound bool
-	for _, evt := range evts2 {
-		status := evt.Topics[3].Big().Uint64()
-		if utils.IsExecuted(uint8(status)) {
-			executedEventFound = true
-		}
+	if !s.True(passedEventFound) {
+		_, _ = utils.Simulate(s.client, receipt.BlockNumber, tx.Hash(), utils.AliceKp.CommonAddress())
 	}
-	s.True(executedEventFound)
-
-	destBalanceAfter, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
-	s.Nil(err)
-	//Balance has increased
-	s.Equal(destBalanceAfter.Cmp(destBalanceBefor), 1)
+	//
+	//senderBalAfter, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
+	//s.Nil(err)
+	//s.Equal(senderBalBefore.Cmp(big.NewInt(0).Add(senderBalAfter, amountToDeposit)), 0)
+	//
+	////Wait 30 seconds for relayer to execute
+	//time.Sleep(30 * time.Second)
+	//lp, err = s.client2.LatestBlock()
+	//s.Nil(err)
+	//queryExecute := utils.BuildQuery(s.bridgeAddr, utils.ProposalEvent, receipt.BlockNumber, lp)
+	//s.Nil(err)
+	//evts2, err := s.client2.Client.FilterLogs(context.Background(), queryExecute)
+	//var executedEventFound bool
+	//for _, evt := range evts2 {
+	//	status := evt.Topics[3].Big().Uint64()
+	//	if utils.IsExecuted(uint8(status)) {
+	//		executedEventFound = true
+	//	}
+	//}
+	//s.True(executedEventFound)
+	//
+	//destBalanceAfter, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
+	//s.Nil(err)
+	////Balance has increased
+	//s.Equal(destBalanceAfter.Cmp(destBalanceBefor), 1)
 }
 
-func (s *IntegrationTestSuite) TestMultipleTransactionsInBlock() {
-	eveSender, err := client.NewClient(TestEndpoint, false, utils.EveKp, big.NewInt(utils.DefaultGasLimit), big.NewInt(utils.DefaultGasPrice))
-	s.Nil(err)
-
-	dstAddr := utils.BobKp.CommonAddress()
-	s.Nil(err)
-	erc20Contract, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client.Client)
-	s.Nil(err)
-	erc20Contract2, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client2.Client)
-	s.Nil(err)
-	senderBalBefore, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
-	s.Nil(err)
-	destBalanceBefor, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
-	s.Nil(err)
-
-	amountToDeposit := big.NewInt(1000000)
-
-	for i := 0; i <= 20; i += 1 {
-		go sendOneWeiWithDelay(eveSender)
-	}
-
-	resourceID := utils.SliceTo32Bytes(append(common.LeftPadBytes(s.erc20ContractAddr.Bytes(), 31), uint8(5)))
-	tx, err := utils.MakeErc20Deposit(s.client, s.bridgeAddr, dstAddr, amountToDeposit, resourceID, 1)
-	s.Nil(err)
-	receipt, err := utils.WaitAndReturnTxReceipt(s.client, tx)
-	s.Nil(err)
-	log.Debug().Msg(tx.Hash().String())
-
-	//Wait 30 seconds for relayer vote
-	time.Sleep(30 * time.Second)
-
-	lp, err := s.client2.LatestBlock()
-	s.Nil(err)
-	// wait for vote log event
-	query := utils.BuildQuery(s.bridgeAddr, utils.ProposalEvent, receipt.BlockNumber, lp)
-	evts, err := s.client2.Client.FilterLogs(context.Background(), query)
-	var passedEventFound bool
-	for _, evt := range evts {
-		status := evt.Topics[3].Big().Uint64()
-		if utils.IsPassed(uint8(status)) {
-			passedEventFound = true
-		}
-	}
-	s.True(passedEventFound)
-
-	senderBalAfter, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
-	s.Nil(err)
-	s.Equal(senderBalBefore.Cmp(big.NewInt(0).Add(senderBalAfter, amountToDeposit)), 0)
-
-	//Wait 30 seconds for relayer to execute
-	time.Sleep(30 * time.Second)
-	lp, err = s.client2.LatestBlock()
-	s.Nil(err)
-	queryExecute := utils.BuildQuery(s.bridgeAddr, utils.ProposalEvent, receipt.BlockNumber, lp)
-	s.Nil(err)
-	evts2, err := s.client2.Client.FilterLogs(context.Background(), queryExecute)
-	var executedEventFound bool
-	for _, evt := range evts2 {
-		status := evt.Topics[3].Big().Uint64()
-		if utils.IsExecuted(uint8(status)) {
-			executedEventFound = true
-		}
-	}
-	s.True(executedEventFound)
-
-	destBalanceAfter, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
-	s.Nil(err)
-	//Balance has increased
-	s.Equal(destBalanceAfter.Cmp(destBalanceBefor), 1)
-}
+//
+//func (s *IntegrationTestSuite) TestMultipleTransactionsInBlock() {
+//	eveSender, err := client.NewClient(TestEndpoint, false, utils.EveKp, big.NewInt(utils.DefaultGasLimit), big.NewInt(utils.DefaultGasPrice))
+//	s.Nil(err)
+//
+//	dstAddr := utils.BobKp.CommonAddress()
+//	s.Nil(err)
+//	erc20Contract, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client.Client)
+//	s.Nil(err)
+//	erc20Contract2, err := erc20.NewERC20PresetMinterPauser(s.erc20ContractAddr, s.client2.Client)
+//	s.Nil(err)
+//	senderBalBefore, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
+//	s.Nil(err)
+//	destBalanceBefor, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
+//	s.Nil(err)
+//
+//	amountToDeposit := big.NewInt(1000000)
+//
+//	for i := 0; i <= 20; i += 1 {
+//		go sendOneWeiWithDelay(eveSender)
+//	}
+//
+//	resourceID := utils.SliceTo32Bytes(append(common.LeftPadBytes(s.erc20ContractAddr.Bytes(), 31), uint8(5)))
+//	tx, err := utils.MakeErc20Deposit(s.client, s.bridgeAddr, dstAddr, amountToDeposit, resourceID, 1)
+//	s.Nil(err)
+//	receipt, err := utils.WaitAndReturnTxReceipt(s.client, tx)
+//	s.Nil(err)
+//	log.Debug().Msg(tx.Hash().String())
+//
+//	//Wait 30 seconds for relayer vote
+//	time.Sleep(30 * time.Second)
+//
+//	lp, err := s.client2.LatestBlock()
+//	s.Nil(err)
+//	// wait for vote log event
+//	query := utils.BuildQuery(s.bridgeAddr, utils.ProposalEvent, receipt.BlockNumber, lp)
+//	evts, err := s.client2.Client.FilterLogs(context.Background(), query)
+//	var passedEventFound bool
+//	for _, evt := range evts {
+//		status := evt.Topics[3].Big().Uint64()
+//		if utils.IsPassed(uint8(status)) {
+//			passedEventFound = true
+//		}
+//	}
+//	s.True(passedEventFound)
+//
+//	senderBalAfter, err := erc20Contract.BalanceOf(s.client.CallOpts(), utils.AliceKp.CommonAddress())
+//	s.Nil(err)
+//	s.Equal(senderBalBefore.Cmp(big.NewInt(0).Add(senderBalAfter, amountToDeposit)), 0)
+//
+//	//Wait 30 seconds for relayer to execute
+//	time.Sleep(30 * time.Second)
+//	lp, err = s.client2.LatestBlock()
+//	s.Nil(err)
+//	queryExecute := utils.BuildQuery(s.bridgeAddr, utils.ProposalEvent, receipt.BlockNumber, lp)
+//	s.Nil(err)
+//	evts2, err := s.client2.Client.FilterLogs(context.Background(), queryExecute)
+//	var executedEventFound bool
+//	for _, evt := range evts2 {
+//		status := evt.Topics[3].Big().Uint64()
+//		if utils.IsExecuted(uint8(status)) {
+//			executedEventFound = true
+//		}
+//	}
+//	s.True(executedEventFound)
+//
+//	destBalanceAfter, err := erc20Contract2.BalanceOf(s.client2.CallOpts(), dstAddr)
+//	s.Nil(err)
+//	//Balance has increased
+//	s.Equal(destBalanceAfter.Cmp(destBalanceBefor), 1)
+//}
